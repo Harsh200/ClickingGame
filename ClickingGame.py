c92a132fa51f656d1f1b7a0b1849875de671473e
Harsh kumar saxena <harsh.saxena_ccv17@gla.ac.in>
2019-07-25 11:25:24 +0530

    Add score command to mainMenu

--- ClickingGame.py
+++ ClickingGame.py
@@ -11,6 +11,7 @@ mainMenu=Menu(window)
 window.config(menu=mainMenu)
 mainMenu.add_command(label="New Game",command=newGame)
 mainMenu.add_separator()
+mainMenu.add_command(label="Score=0")
 
 
 window.mainloop()
